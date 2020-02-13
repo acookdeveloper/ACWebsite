eeaa13492c0ea9b858bdd114637c7b9ac6a5f25a
Anthony Cook <AnthonyCook@smasltd.com>
2020-02-13 15:37:10 +0000

    adding header for User Agent - this was why the API call didn't work

--- ACWebsite/Controllers/CryptoController.cs
+++ ACWebsite/Controllers/CryptoController.cs
@@ -1,12 +1,9 @@
 ﻿using ACWebsite.Models;
 using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
-using System.Web;
 using System.Web.Mvc;
 
 namespace ACWebsite.Controllers
@@ -24,32 +21,31 @@ namespace ACWebsite.Controllers
         {
             string Baseurl = "https://api.pro.coinbase.com/";
 
-            returnStats EmpInfo = new returnStats();
+            returnStats LTCInfo = new returnStats();
 
             using (var client = new HttpClient())
             {
                 //Passing service base url  
                 client.BaseAddress = new Uri(Baseurl);
-
+            
                 client.DefaultRequestHeaders.Clear();
                 //Define request data format  
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-                //Sending request to find web api REST service resource GetAllEmployees using HttpClient  
+                client.DefaultRequestHeaders.Add("User-Agent", "AC Website C# MVC");
+                //Sending request to find the LTC GBP stats  
                 HttpResponseMessage Res = await client.GetAsync("products/LTC-GBP/stats");
-
-                //Checking the response is successful or not which is sent using HttpClient  
+                //Checking the response is successful or not
                 if (Res.IsSuccessStatusCode)
                 {
                     //Storing the response details recieved from web api   
-                    var EmpResponse = Res.Content.ReadAsStringAsync().Result;
-
-                    //Deserializing the response recieved from web api and storing into the Employee list  
-                    EmpInfo = JsonConvert.DeserializeObject<returnStats>(EmpResponse);
-
+                    var LTCResponse = Res.Content.ReadAsStringAsync().Result;
+            
+                    //Deserializing the response recieved from web api and storing into the Object 
+                    LTCInfo = JsonConvert.DeserializeObject<returnStats>(LTCResponse);
+            
                 }
                 //returning the employee list to view  
-                return View(EmpInfo);
+                return View(LTCInfo);
             }
         }
     }
